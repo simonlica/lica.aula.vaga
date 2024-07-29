0330df12de206fd8c1dc3c2493f374f4d74f4be3
Lica Simon <carvalho.heloisa@escola.pr.gov.br>
2024-07-29 10:25:30 -0300

    first commit

---
+++ express_lica/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("<h1>Olá ExpressJS.</h1>");
+});
+server.get("/sobre", (req, res) => {
+  res.send("<h2>Somos quem podemos ser.</h2>");
+});
+
+server.get("/json", (req, res) => {
+  res.send({ nome: "Alcebíades" });
+});
+
+server.use((req, res) => {
+  // res.send("Erro !!!")
+  res.status(404).send("<h1>ERRO!</h1>");
+});
+
+server.listen(3000);
